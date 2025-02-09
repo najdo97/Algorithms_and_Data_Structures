47f70d714be184bfefff98dc67ed622f55bd13fa
Borjan Najdoski <najdoskib@gmail.com>
2025-02-09 23:47:07 +0100

    Ispitna

--- 2_Kolokvium/Zadaca1/src/Main.java
+++ 2_Kolokvium/Zadaca1/src/Main.java
@@ -1,5 +1,313 @@
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+import java.util.*;
+
+ class SLLNode<E> {
+    protected E element;
+    protected SLLNode<E> succ;
+
+    public SLLNode(E elem, SLLNode<E> succ) {
+        this.element = elem;
+        this.succ = succ;
+    }
+
+    @Override
+    public String toString() {
+        return element.toString();
     }
 }
+
+ class MapEntry<K, E> {
+    // Each MapEntry object is a pair consisting of a key
+    // and a value (an arbitrary object).
+    K key;
+    E value;
+
+    public MapEntry(K key, E val) {
+        this.key = key;
+        this.value = val;
+    }
+
+    public String toString() {
+        return "<" + key + "," + value + ">";
+    }
+}
+
+class CBHT<K, E> {
+    // An object of class CBHT is a closed-bucket hash table, containing
+    // entries of class MapEntry.
+    private SLLNode<MapEntry<K, E>>[] buckets;
+
+    @SuppressWarnings("unchecked")
+    public CBHT(int m) {
+        // Construct an empty CBHT with m buckets.
+        buckets = (SLLNode<MapEntry<K, E>>[]) new SLLNode[m];
+    }
+
+    private int hash(K key) {
+        // Translate key to an index of the array buckets.
+        return Math.abs(key.hashCode()) % buckets.length;
+    }
+
+    public SLLNode<MapEntry<K, E>> search(K targetKey) {
+        // Find which if any node of this CBHT contains an entry whose key is equal to targetKey.
+        // Return a link to that node (or null if there is none).
+        int b = hash(targetKey);
+        SLLNode<MapEntry<K, E>> currNode = buckets[b];
+        while (currNode != null) {
+            MapEntry<K, E> currEntry = currNode.element;
+            if (currEntry.key.equals(targetKey)) return currNode;
+            else currNode = currNode.succ;
+        }
+        return null;
+    }
+
+    public void insert(K key, E val) {
+        // Insert the entry <key, val> into this CBHT.
+        // If entry with same key exists, overwrite it.
+        MapEntry<K, E> newEntry = new MapEntry<>(key, val);
+        int b = hash(key);
+        SLLNode<MapEntry<K, E>> currNode = buckets[b];
+        while (currNode != null) {
+            MapEntry<K, E> currEntry = currNode.element;
+            if (currEntry.key.equals(key)) {
+                // Make newEntry replace the existing entry ...
+                currNode.element = newEntry;
+                return;
+            } else currNode = currNode.succ;
+        }
+        // Insert newEntry at the front of the SLL in bucket b ...
+        buckets[b] = new SLLNode<>(newEntry, buckets[b]);
+    }
+
+    public void delete(K key) {
+        // Delete the entry (if any) whose key is equal to key from this CBHT.
+        int b = hash(key);
+        SLLNode<MapEntry<K, E>> predNode = null, currNode = buckets[b];
+        while (currNode != null) {
+            MapEntry<K, E> currEntry = currNode.element;
+            if (currEntry.key.equals(key)) {
+                if (predNode == null) buckets[b] = currNode.succ;
+                else predNode.succ = currNode.succ;
+                return;
+            } else {
+                predNode = currNode;
+                currNode = currNode.succ;
+            }
+        }
+    }
+
+    public String toString() {
+        String temp = "";
+        for (int i = 0; i < buckets.length; i++) {
+            temp += i + ":";
+            SLLNode<MapEntry<K, E>> curr = buckets[i];
+            while (curr != null) {
+                temp += curr.element.toString() + " ";
+                curr = curr.succ;
+            }
+            temp += "\n";
+        }
+        return temp;
+    }
+}
+
+class OBHT<K, E> {
+
+    // An object of class OBHT is an open-bucket hash table, containing entries
+    // of class MapEntry.
+    private MapEntry<K, E>[] buckets;
+
+    // buckets[b] is null if bucket b has never been occupied.
+    // buckets[b] is former if bucket b is formerly-occupied
+    // by an entry that has since been deleted (and not yet replaced).
+
+    static final int NONE = -1; // ... distinct from any bucket index.
+
+    private final MapEntry<K, E> former = new MapEntry<>(null, null);
+    // This guarantees that, for any genuine entry e,
+    // e.key.equals(former.key) returns false.
+
+    private int occupancy = 0;
+    // ... number of occupied or formerly-occupied buckets in this OBHT.
+
+    @SuppressWarnings("unchecked")
+    public OBHT(int m) {
+        // Construct an empty OBHT with m buckets.
+        buckets = (MapEntry<K, E>[]) new MapEntry[m];
+    }
+
+
+    private int hash(K key) {
+        // Translate key to an index of the array buckets.
+        return Math.abs(key.hashCode()) % buckets.length;
+    }
+
+
+    public int search(K targetKey) {
+        // Find which if any bucket of this OBHT is occupied by an entry whose key
+        // is equal to targetKey. Return the index of that bucket.
+        int b = hash(targetKey);
+
+        for (int n_search = 0; n_search < buckets.length; n_search++) {
+            MapEntry<K, E> currEntry = buckets[b];
+            if (currEntry == null) return NONE;
+            else if (currEntry != former && currEntry.key.equals(targetKey)) return b;
+            else b = (b + 1) % buckets.length;
+        }
+        return NONE;
+    }
+
+    public MapEntry<K, E> getBucket(int bucket) {
+        return buckets[bucket];
+    }
+
+
+    public void insert(K key, E val) {
+        // Insert the entry <key, val> into this OBHT.
+        MapEntry<K, E> newEntry = new MapEntry<K, E>(key, val);
+        int b = hash(key);
+        int n_search = 0;
+        Integer free_position = null;
+        Integer existing_position = null;
+        while (n_search < buckets.length) {
+            MapEntry<K, E> currEntry = buckets[b];
+            if (currEntry == null) {
+                if (free_position == null) free_position = b;
+                break;
+            } else if (currEntry != former && currEntry.key.equals(key)) {
+                existing_position = b;
+                break;
+            } else {
+                if (currEntry == former && free_position == null) {
+                    free_position = b;
+                }
+                b = (b + 1) % buckets.length;
+                n_search++;
+            }
+        }
+        if (existing_position != null)
+            buckets[existing_position] = newEntry;
+        else if (free_position != null) {
+            buckets[free_position] = newEntry;
+            if (++occupancy == buckets.length) {
+                System.out.println("Hash tabelata e polna!!!");
+            }
+        } else {
+            // failed to insert, table was already full
+        }
+    }
+
+
+    @SuppressWarnings("unchecked")
+    public void delete(K key) {
+        // Delete the entry (if any) whose key is equal to key from this OBHT.
+        int b = hash(key);
+        int n_search = 0;
+        while (n_search < buckets.length) {
+            MapEntry<K, E> currEntry = buckets[b];
+            if (currEntry == null) return;
+            else if (currEntry != former && currEntry.key.equals(key)) {
+                buckets[b] = former;
+                --occupancy;
+                return;
+            } else {
+                b = (b + 1) % buckets.length;
+                n_search++;
+            }
+        }
+    }
+
+
+    public String toString() {
+        String temp = "";
+        for (int i = 0; i < buckets.length; i++) {
+            temp += i + ":";
+            if (buckets[i] == null)
+                temp += "\n";
+            else if (buckets[i] == former)
+                temp += "former\n";
+            else
+                temp += buckets[i] + "\n";
+        }
+        return temp;
+    }
+
+
+    public OBHT<K, E> clone() {
+        OBHT<K, E> copy = new OBHT<K, E>(buckets.length);
+        for (int i = 0; i < buckets.length; i++) {
+            MapEntry<K, E> e = buckets[i];
+            if (e != null && e != former)
+                copy.buckets[i] = new MapEntry<>(e.key, e.value);
+            else if (e == former)
+                copy.buckets[i] = copy.former;
+            else
+                copy.buckets[i] = null;
+        }
+        return copy;
+    }
+}
+
+
+
+// Следните класи веќе се импортирани, не е дозволено копирање на класи овде, директно користејте ги како кога се достапни во други локални фајлови:
+// The following classes are already imported, copying classes here is not allowed, use them directly as when they are available in other local files:
+
+// CBHT, OBHT, MapEntry, SLLNode веќе се импортирани
+// CBHT, OBHT, MapEntry, SLLNode are already imported
+
+// Овде креирајте ги помошните класи за клуч и вредност
+// Исполнете ги барањата од текстот за toString методите
+// Дополнително осигурете се дека вашата клуч класа ќе ги имплементира потребните
+// hashCode и equals методи
+
+// Create the helper classes for key and value here
+// Fulfill the requirements from the text for the toString methods
+// Additionally, make sure that your key class will implement the required
+// hashCode and equals methods
+class Person {
+    @Override
+    public String toString() {
+
+    }
+
+
+    // имплементирајте ги следните два методи за да работи табелата правилно
+    // implement the following two methods to make the table work properly
+    @Override
+    public boolean equals(Object o) {
+
+    }
+
+    @Override
+    public int hashCode() {
+
+    }
+}
+
+class Project {
+
+    }
+
+
+    @Override
+    public String toString() {
+
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+        // Креирајте ја табелата според барањата
+        // Create the table as requested
+        ...table = new ...;
+
+        // Прочитајте ги податоците од влезот и пополнете ја табелата
+        // Read the input data and fill the table
+
+        // отпечатете ја вашата табела
+        // print your table
+        System.out.println(table);
+    }
+}
+
+
---
+++ ispitnaAips/src/Main.java
@@ -0,0 +1,311 @@
+import java.util.*;
+
+
+interface Stack<E> {
+    // Elementi na stekot se objekti od proizvolen tip.
+    // Metodi za pristap:
+
+    public boolean isEmpty();
+    // Vrakja true ako i samo ako stekot e prazen.
+
+    public E peek();
+    // Go vrakja elementot na vrvot od stekot.
+
+    // Metodi za transformacija:
+    public void clear();
+    // Go prazni stekot.
+
+    public void push(E x);
+    // Go dodava x na vrvot na stekot.
+
+    public E pop();
+    // Go otstranuva i vrakja elementot shto e na vrvot na stekot.
+}
+
+class SLLNode<E> {
+    protected E element;
+    protected SLLNode<E> succ;
+
+    public SLLNode(E elem, SLLNode<E> succ) {
+        this.element = elem;
+        this.succ = succ;
+    }
+
+    @Override
+    public String toString() {
+        return element.toString();
+    }
+}
+
+
+class LinkedStack<E> implements Stack<E> {
+    // top e link do prviot jazol ednostrano-povrzanata lista koja sodrzi gi elementite na stekot .
+    private SLLNode<E> top;
+    int size;
+
+    public LinkedStack() {
+        // Konstrukcija na nov, prazen stek.
+        top = null;
+        size = 0;
+    }
+
+    public String toString() {
+        SLLNode<E> current = top;
+        StringBuilder s = new StringBuilder();
+        while (current != null) {
+            s.append(current.element);
+            s.append(" ");
+            current = current.succ;
+        }
+        return s.toString();
+    }
+
+    public boolean isEmpty() {
+        // Vrakja true ako i samo ako stekot e prazen.
+        return (top == null);
+    }
+
+    public void clear() {
+        // Go prazni stekot.
+        top = null;
+        size = 0;
+    }
+
+    public E peek() {
+        // Go vrakja elementot na vrvot od stekot.
+        if (top == null)
+            throw new NoSuchElementException();
+        return top.element;
+    }
+
+    public void push(E x) {
+        // Go dodava x na vrvot na stekot.
+        top = new SLLNode<E>(x, top);
+        size++;
+    }
+
+    public int size() {
+        // Ja vrakja dolzinata na stekot.
+        return size;
+    }
+
+    public E pop() {
+        // Go otstranuva i vrakja elementot shto e na vrvot na stekot.
+        if (top == null)
+            throw new NoSuchElementException();
+        E topElem = top.element;
+        size--;
+        top = top.succ;
+        return topElem;
+    }
+
+}
+
+
+//  \begin{section}
+//    \begin{subsection}
+//        \begin{subsubsection}
+//            \begin{text}
+//             \end{text}
+//        \end{subsubsection}
+//    \end{subsection}
+//\end{section}
+
+
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int num = sc.nextInt();
+        sc.nextLine();      //todo - bigest problem
+
+        LinkedStack<String> stack = new LinkedStack<>();
+        boolean answer = true;
+
+aaaaaaa        while (sc.hasNextLine()) {
+
+            String input = sc.nextLine().trim();
+
+            if(input.isBlank()){        //todo - ako e prazna linijata, kraj na ciklusot
+                break;
+            }
+
+            if (!stack.isEmpty()) {
+
+                if (stack.peek().equals("\\begin{section}")) {
+
+                    if (
+                            input.equals("\\end{subsection}") ||
+                            input.equals("\\end{subsubsection}") ||
+                            input.equals("\\end{text}") ||
+                            !input.startsWith("\\")
+                    ) {
+                        answer = false;
+                        break;
+                    }
+                } else if (stack.peek().equals("\\begin{subsection}")) {
+
+                    if (
+                            input.equals("\\begin{section}") ||
+                            input.equals("\\end{section}") ||
+                            input.equals("\\end{subsubsection}") ||
+                            input.equals("\\end{text}") ||
+                            !input.startsWith("\\")
+                    ) {
+                        answer = false;
+                        break;
+                    }
+
+                } else if (stack.peek().equals("\\begin{subsubsection}")) {
+                    if (
+                            input.equals("\\begin{section}") ||
+                            input.equals("\\end{section}") ||
+                            input.equals("\\begin{subsection}") ||
+                            input.equals("\\end{subsection}") ||
+                            input.equals("\\end{text}") ||
+                            !input.startsWith("\\")
+                    ) {
+                        answer = false;
+                        break;
+                    }
+
+
+                } else if (stack.peek().equals("\\begin{text}")) {
+                    if (
+                            input.equals("\\begin{section}") ||
+                            input.equals("\\end{section}") ||
+                            input.equals("\\begin{subsection}") ||
+                            input.equals("\\end{subsection}") ||
+                            input.equals("\\begin{subsubsection}") ||
+                            input.equals("\\end{subsubsection}")
+                    ) {
+                        answer = false;
+                        break;
+                    }
+                }
+            }
+
+            if (input.startsWith("\\end") && stack.isEmpty()) {         //todo - ako pocneme da vnesuvame prvicno \end{...} elementi
+                answer = false;
+                break;
+            }
+
+            if (input.startsWith("\\end")) {
+                if (!stack.peek().startsWith("\\")) {
+                    if (!stack.isEmpty()) {
+                        stack.pop();            //ako e tekst
+                    } else {
+                        answer = false;
+                        break;
+                    }
+                }
+                if (!stack.isEmpty()) {
+                    stack.pop();        // ako e obicen tak
+                } else {
+                    {
+                        answer = false;
+                        break;
+                    }
+                }
+            } else stack.push(input);
+
+        }
+
+        if (!stack.isEmpty()) {
+            answer = false;
+        }
+
+        System.out.println(answer);
+    }
+}
+
+
+//5
+//\begin{section}
+//\begin{text}
+//text
+//\end{text}
+//\end{section}
+
+//TRUE
+//------------------------------------------------------------------------------------------------------------------
+
+
+//4
+//\begin{section}
+//\begin{text}
+//text
+//\end{text}
+
+//FALSE
+//----------------------------------------------------------
+
+
+//9
+//\begin{section}
+//\begin{subsection}
+//\begin{subsubsection}
+//\begin{text}
+//text
+//\end{text}
+//\end{subsubsection}
+//\end{subsection}
+//\end{section}
+
+//TRUE
+//--------------------------------------------------------------
+
+
+//15
+//\begin{section}
+//\begin{subsection}
+//\begin{subsubsection}
+//\begin{text}
+//text
+//\end{text}
+//\end{subsubsection}
+//\begin{text}
+//text
+//\end{text}
+//\end{subsection}
+//\begin{text}
+//text
+//\end{text}
+//\end{section}
+//TRUE
+//--------------------------------------------------------------
+
+
+//14
+//\begin{section}
+//\begin{subsection}
+//\begin{subsubsection}
+//\begin{text}
+//text
+//\end{text}
+//\end{subsubsection}
+//\begin{text}
+//text
+//\end{text}
+//\end{subsection}
+//\begin{text}
+//text
+//\end{text}
+//FALSE
+//--------------------------------------------------------------
+
+
+//9
+//\end{text}
+//\end{subsubsection}
+//\begin{text}
+//text
+//\end{text}
+//\end{subsection}
+//\begin{text}
+//text
+//\end{text}
+//FALSE
+
+//--------------------------------------------------------------
