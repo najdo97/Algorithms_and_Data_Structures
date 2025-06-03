0fb6c20f2e21b3ebcd5150759bc97d8bc30c8652
Borjan Najdoski <najdoskib@gmail.com>
2025-06-03 17:15:50 +0200

    One dimentisonal zadaca 1

---
+++ one_dimensional_data_structures/Zad1/src/main/java/org/example/Main.java
@@ -0,0 +1,117 @@
+package org.example;
+import java.util.Scanner;
+import java.util.NoSuchElementException;
+
+class SLLNode<E> {
+    protected E element;
+    protected SLLNode<E> succ;
+
+    public SLLNode(E elem, SLLNode<E> succ) {
+        this.element = elem;
+        this.succ = succ;
+    }
+
+    @Override
+    public String toString() {
+        return element.toString();
+    }
+}
+
+interface Stack<E> {
+    // Elementi na stekot se objekti od proizvolen tip.
+    // Metodi za pristap:
+
+    public boolean isEmpty();
+    // Vrakja true ako i samo ako stekot e prazen.
+
+    public E peek();
+    // Go vrakja elementot na vrvot od stekot.
+
+    // Metodi za transformacija:
+    public void clear();
+    // Go prazni stekot.
+
+    public void push(E x);
+    // Go dodava x na vrvot na stekot.
+
+    public E pop();
+    // Go otstranuva i vrakja elementot shto e na vrvot na stekot.
+}
+
+class LinkedStack<E> implements Stack<E> {
+    // top e link do prviot jazol ednostrano-povrzanata lista koja sodrzi gi elementite na stekot .
+    private SLLNode<E> top;
+    int size;
+
+    public LinkedStack() {
+        // Konstrukcija na nov, prazen stek.
+        top = null;
+        size = 0;
+    }
+
+    public String toString() {
+        SLLNode<E> current = top;
+        StringBuilder s = new StringBuilder();
+        while (current != null) {
+            s.append(current.element);
+            s.append(" ");
+            current = current.succ;
+        }
+        return s.toString();
+    }
+
+    public boolean isEmpty() {
+        // Vrakja true ako i samo ako stekot e prazen.
+        return (top == null);
+    }
+
+    public void clear() {
+        // Go prazni stekot.
+        top = null;
+        size = 0;
+    }
+
+    public E peek() {
+        // Go vrakja elementot na vrvot od stekot.
+        if (top == null)
+            throw new NoSuchElementException();
+        return top.element;
+    }
+
+    public void push(E x) {
+        // Go dodava x na vrvot na stekot.
+        top = new SLLNode<E>(x, top);
+        size++;
+    }
+
+    public int size() {
+        // Ja vrakja dolzinata na stekot.
+        return size;
+    }
+
+    public E pop() {
+        // Go otstranuva i vrakja elementot shto e na vrvot na stekot.
+        if (top == null)
+            throw new NoSuchElementException();
+        E topElem = top.element;
+        size--;
+        top = top.succ;
+        return topElem;
+    }
+
+}
+
+
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner in = new Scanner(System.in);
+
+        while (!in.next().equals("x")) {
+
+
+        }
+
+
+    }
+}
